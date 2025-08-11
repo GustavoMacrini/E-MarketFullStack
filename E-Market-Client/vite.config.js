f6edfc97e070be231733ddd4bd140a27dfae966a
Gustavo Macrini <82122229+GustavoMacrini@users.noreply.github.com>
2025-08-11 20:10:44 -0300

    docker-compose + front

---
+++ E-Market-Client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [plugin()],
+    server: {
+        host: true,
+        port: 55786,
+        watch: {
+            usePolling: true,
+        },
+    },
+})
